f2ed3fb00d947047eb6305fa989a735fc02e11ed
MD Manning <manningmichaeld@johndeere.com>
2020-11-13 15:48:02 -0600

    30 rack

--- plantapp_backend/models/users.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isActive: DataTypes.BOOLEAN
-  }, {});
-  return User;
-};
